9a48ac78fa482f066599f357cc9589011251ff15
Amelia <apottr@Amelias-MacBook-Pro.local>
2017-09-20 20:14:47 -0400

    error: no returned words in stopwordsHeuristic

--- main.go
+++ main.go
@@ -4,10 +4,25 @@ import (
   "strings"
   "fmt"
   "regexp"
+  "io/ioutil"
+  "sort"
 )
 
 func stopwordsHeuristic(data string){
-  
+  sw,err := ioutil.ReadFile("stop-word-list.txt")
+  out := []string{}
+  if err != nil {
+    panic(err)
+  }
+  lines := strings.Split(string(sw),"\n")
+  for _,word := range strings.Split(data," ") {
+    fmt.Println(word)
+    if (idx := sort.SearchStrings(lines,word);
+        idx != len(lines) && lines[idx] == word) {
+      out = append(out,word)
+    }
+  }
+  fmt.Printf("%q",out)
 }
 
 func capHeuristic(data string){
@@ -24,5 +39,5 @@ func capHeuristic(data string){
 }
 
 func main(){
-  capHeuristic(`Bruce McLaren Motorsport Park (previously known as the Taupo Motorsport Park) is a motorsports circuit located in Broadlands Road, Taupo, New Zealand. It is owned and operated by MIT Development Ltd.`)
+  stopwordsHeuristic(`Bruce McLaren Motorsport Park (previously known as the Taupo Motorsport Park) is a motorsports circuit located in Broadlands Road, Taupo, New Zealand. It is owned and operated by MIT Development Ltd.`)
 }
